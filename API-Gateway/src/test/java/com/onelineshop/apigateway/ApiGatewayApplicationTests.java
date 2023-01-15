875c41a25e8bfc1f0e09db954640f15681d48c72
sanit7722 <sanit7722@gmail.com>
2023-01-15 20:57:59 +0530

    Implemented API Gateway

---
+++ API-Gateway/src/test/java/com/onelineshop/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onelineshop.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
